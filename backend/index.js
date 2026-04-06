ee56c15077c930c9c2ab88f0d7d1dfed6193e70e
Aoarna1-ag <aparnagupta.csa@gmail.com>
2026-04-06 18:59:39 +0530

    fetching list of all files from bucket, uploading multiple files at once and images rendering in grid

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,23 @@
 import express from "express";
 import multer from "multer";
 import dotenv from "dotenv";
+import cors from "cors";
 import {
   S3Client,
   PutObjectCommand,
   GetObjectCommand,
+  ListObjectsV2Command,
 } from "@aws-sdk/client-s3";
 
 dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 
 // console.log(process.env.AWS_ACCESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY);
 
@@ -24,38 +34,67 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 // const upload = multer({ dest: "upload/" });
 
-const app = express();
-
 const PORT = 3000;
 
 app.get("/hello", (req, res) => {
   res.send("app listening");
 });
 
-app.post("/upload", upload.single("file"), async (req, res) => {
-  if (!req.file) res.status(400).json({ message: "No file uploaded!" });
+app.post("/upload", upload.array("file"), async (req, res) => {
+  if (!req.files?.length)
+    res.status(400).json({ message: "No file uploaded!" });
 
   // res.status(200).json({ message: "File uploaded successfully!" });
 
   //   console.log(req.file);
   //   originalname mimetype
 
-  const command = new PutObjectCommand({
+  // console.log(req.files);
+
+  try {
+    for (let each of req.files) {
+      const command = new PutObjectCommand({
+        Bucket: process.env.AWS_S3_BUCKET,
+        Key: each.originalname,
+        Body: each.buffer,
+        ContentType: each.mimetype,
+      });
+
+      await client.send(command);
+    }
+
+    res.status(200).json({ message: "Uploaded successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+app.get("/list", async (req, res) => {
+  const command = new ListObjectsV2Command({
     Bucket: process.env.AWS_S3_BUCKET,
-    Key: req.file.originalname,
-    Body: req.file.buffer,
-    ContentType: req.file.mimetype,
   });
 
-  const uploadResponse = await client.send(command);
+  try {
+    const response = await client.send(command);
 
-  res.status(200).json({ uploadResponse });
+    console.log(response);
+
+    const fileKeys = response.Contents.map((item) => item.Key);
+    res.status(200).json({ data: fileKeys });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 app.get("/image", async (req, res) => {
+  const fileName = req.query.name;
+
+  console.log(fileName);
+
   const command = new GetObjectCommand({
     Bucket: process.env.AWS_S3_BUCKET,
-    Key: "tea-rose-garden-roses-plants-stem-flowers.webp",
+    Key: fileName,
     // Key: "Screenshot 2025-12-11 at 22.44.53.png",
   });
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,75 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+const serverName = `http://localhost:3000`;
+
 function App() {
+  const [carsList, setCarsList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(false);
+
+  const fetchCarsList = async () => {
+    setLoading(true);
+    try {
+      const url = `${serverName}/list`;
+      const res = await fetch(url);
+      const list = await res.json();
+      console.log(list);
+      setCarsList(list.data);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCarsList();
+  }, []);
+
   return (
     <>
-      Hello
-      <img src="http://localhost:3000/image" />
-      {/* <video src="http://localhost:3000/video" width={700} autoPlay /> */}
+      <h1 style={{ margin: "3rem" }}> Hottest Cars in 2026</h1>
+      {/* <img src="http://localhost:3000/image" /> */}
+      {loading ? (
+        <div> Loading...</div>
+      ) : carsList.length ? (
+        <div
+          style={{
+            margin: "auto",
+            width: "90vw",
+
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: "repeat(2, max-content)",
+              width: "100%",
+              justifyContent: "space-between",
+
+              gap: "45px",
+            }}
+          >
+            {carsList.map((item, i) => (
+              <div key={i}>
+                <img
+                  src={`${serverName}/image?name=${item}`}
+                  style={{
+                    objectFit: "cover",
+                    width: "600px",
+                    height: "400px",
+                  }}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div>No Cars Found</div>
+      )}
     </>
   );
 }
